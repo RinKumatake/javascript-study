9003d0aef605664bb5fd758d89168f6631702513
pandanoranran <pandanoranran658@gmail.com>
2020-04-30 20:31:02 +0900

    対戦表の作成

---
+++ 200430.js
@@ -0,0 +1,21 @@
+'use strict';
+const combination = (array) => {
+    const array2 = [];
+    for(let i = 0; i < array.length -1; i++) {
+        let tmpArray = array.slice(i+1,array.length)
+        for(let j = 0; j < tmpArray.length; j ++) {
+            x = `${array[i]} vs ${tmpArray[j]}`;
+            array2.push(x);
+        }
+    }    
+    return array2
+}
+
+const array1 = ['A', 'B', 'C', 'D', 'E'];
+const combinations = combination(array1);
+for(element of combinations){
+    console.log(element);
+}
+
+
+    
