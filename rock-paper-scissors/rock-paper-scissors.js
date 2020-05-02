6fb86cf6b25132c1c7c605be1db961a8158a81d6
pandanoranran <pandanoranran658@gmail.com>
2020-05-02 10:48:25 +0900

    じゃんけんゲームの結果判定部分を訂正

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -6,23 +6,27 @@
  */
 const rockPaperScissors = (s) => {
     //文字列で与えられたプレイヤーの手を数値化する
+    console.log(s);
     let player = 0;
     if (s == "GU") {
         player = 0;
     } else if(s == "CHOKI") {
         player = 1;
-    } else {
+    } else if(s == "PA") {
         player = 2;
     }
+   
+    let rps = ["GU", "CHOKI", "PA"] //最終結果出力時にインデックス番号と出した手(数字)と照合するための配列
+    let enemy = Math.floor(Math.random()*3); //コンピュータの出す手(0, 1, 2    
+   
+    const result = player - enemy
     
-    let enemy = Math.floor(Math.random()*3); //コンピュータの出す手(0, 1, 2)
-    let rps = ["GU", "CHOKI", "PA"] //配列のインデックス番号をコンピュータの出した手(数字)と照合する
-    if(player > enemy) {
-        return `あなたが${s}、相手が${rps[enemy]}なのであなたの勝ちです`;
-    } else if(player < enemy){
-        return `あなたが${s}、相手が${rps[enemy]}なのであなたの負けです`;
+    if (result == -1 || result == 2){
+        return `あなたが${rps[player]}、相手が${rps[enemy]}なのであなたの勝ちです`;
+    } else if(result == -2 || result == 1){
+        return `あなたが${rps[player]}、相手が${rps[enemy]}なのであなたの負けです`;
     } else {
-        return `あなたが${s}、相手が${rps[enemy]}なのであいこです。もう一度挑戦してください`;
+        return `あなたが${rps[player]}、相手が${rps[enemy]}なのであいこです。もう一度挑戦してください`;
     }
 }
 
