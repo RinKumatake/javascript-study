97e530056ae4b4b3a5149c3d01ace25f4324cbdd
pandanoranran <pandanoranran658@gmail.com>
2020-05-06 16:28:37 +0900

    セミコロンの付け忘れを修正

--- 200502/200502.js
+++ 200502/200502.js
@@ -3,6 +3,6 @@ moji.innerText = 'あいうえお';
 moji.addEventListener('click', function(){
     let color = ['orange', 'green', 'blue', 'red'];
     let number = Math.floor(Math.random()*4)
-    mojiColor = color[number]
+    mojiColor = color[number];
     moji.style.color = mojiColor;
-})
+});
